b67c6155c6e87488f907f7a2fcb56a321ddb994a
OMOCHInoHOSHI <ittamago2020kai@gmail.com>
2026-07-01 14:41:19 +0900

    update: event_membersテーブルにcreated_atを追加 #52

--- db/migrations/20260626045435_create_event_members.sql
+++ db/migrations/20260626045435_create_event_members.sql
@@ -3,8 +3,10 @@ CREATE TABLE event_members (
     id UUID PRIMARY KEY,
     event_id UUID NOT NULL   REFERENCES events(id),
     profile_id UUID REFERENCES profiles(id),
-    username TEXT NOT NULL,         
-    mail_address TEXT NOT NULL
+    username TEXT NOT NULL,
+    mail_address TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
 );
+
 -- +goose Down
 DROP TABLE event_members;
